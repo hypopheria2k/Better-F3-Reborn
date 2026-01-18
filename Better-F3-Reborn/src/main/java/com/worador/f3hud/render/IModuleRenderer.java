b0ebb0a2a51e9a04c9fc0e8066bf3582139d57a9
hypopheria2k <hypopheria@googlemail.com>
2026-01-18 19:14:15 +0100

    Fix: Remove embedded repository link and add files properly

--- Better-F3-Reborn/src/main/java/com/worador/f3hud/render/IModuleRenderer.java
+++
@@ -1,7 +0,0 @@
-package com.worador.f3hud.render;
-
-import com.worador.f3hud.InfoModule;
-
-public interface IModuleRenderer {
-    void render(InfoModule module, int x, int y, float animProgress);
-}
